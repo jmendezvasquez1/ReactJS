c382a9b0e0d8c2516ef1da288bcc202636cb3a95
Jairo <mjaioariel@gmail.com>
2019-06-07 11:45:18 -0600

    Solo lo necesario

--- ReactJS Proyect/integracion/src/Components/Contenido1.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class Contenido1 extends React.Component{
-    render(){
-    return(<h1>Contenido1</h1>)}
-} 
-ReactDOM.render(Contenido1,document.getElementById("contenido"));
-export default Contenido1
